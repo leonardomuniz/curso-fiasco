246c5c1e8aa67f06dc714406f1f17f62c52c6879
leonardomuniz <leocontato@outlook.com.br>
2026-04-06 21:47:20 -0300

    first commit

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,8 +1,15 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 public class Controller {
 
+    @GetMapping("/")
+    public String boasVindas() {
+        return "Essa é a mensagem de boas vindas";
+    }
 }
